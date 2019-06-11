6934fd3155e1f3250574317ed29a7344549ada3e
marun <norihiro.maruyama@gmail.com>
2019-06-11 17:20:05 +0900

    add class test code.

--- src/index.js
+++ src/index.js
@@ -9,3 +9,25 @@ function component() {
 }
 
 document.body.appendChild(component());
+
+class  TestClass {
+  constructor() {
+    this.str = ""
+  }
+
+  addString(str) {
+    this.str += str
+    this.str += " "
+  }
+
+  getElement() {
+    let element = document.createElement('div')
+    element.innerHTML = this.str
+    return element
+  }
+}
+
+let a = new TestClass()
+a.addString("test")
+a.addString("class")
+document.body.appendChild(a.getElement())
